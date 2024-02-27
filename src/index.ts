4762d3f75707c977b32ba05d208920c1cea97de2
Kyle Lawson <contact@kylelawson.dev>
2024-02-27 23:57:15 +0000

    [poc] socket.io working example

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { createServer } from "node:http";
 import "dotenv/config";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const PORT = process.env.PORT!;
 
 const app: Express = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -15,6 +17,18 @@ app.get("/", (req: Request, res: Response) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-server.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
 });
